870307551bbf0d91d696010b46e89c0845dd2261
Kep Amun <Kep@Alchem.ie>
2019-04-19 12:57:16 -0400

    Added initial axis UI and stub handler script. Ordered elements by atomic number. Added Silicon and Argon.

---
+++ Assets/ReplaceAtomHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ReplaceAtomHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    AtomController _atomBeingDragged;
+    AtomController _potentialTargetAtom;
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Move atom being dragged.
+        // Find atoms in range for replacement
+        // highlight potential target atom, if any.
+    }
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if(_potentialTargetAtom != null)
+        {
+            _potentialTargetAtom.Element = _atomBeingDragged.Element;
+        }
+
+        Destroy(_atomBeingDragged.gameObject);
+    }
+}
